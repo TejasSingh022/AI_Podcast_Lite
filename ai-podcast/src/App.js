efc2f8b08e1fbc8d8c8783d96c9754e49f110fd0
TejasSingh022 <tejassingh8493@gmail.com>
2025-05-08 00:59:24 +0530

    Initial setup from AI_Podcast repo

--- ai-podcast/src/App.js
+++ ai-podcast/src/App.js
@@ -1,6 +1,5 @@
 import './output.css'; 
 import Home from './pages/home.js';
-import History from './pages/history.js';
 import Navbar from './components/navbar.js';
 import Footer from './components/footer.js';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
